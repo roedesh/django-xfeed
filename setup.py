c1679a9a58c1a6644400e06e1d11e1a575b3f28c
Ruud Schroen <schroenruud@gmail.com>
2015-08-27 13:25:22 +0200

    included tags on company detail

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-xfeed',
-    version='0.0.5',
+    version='0.0.6',
     packages=['xfeed'],
     include_package_data=True,
     license='BSD License',
